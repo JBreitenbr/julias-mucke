f1f7eff126aeaa398db884715705b366949db851
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2024-08-07 09:26:09 +0000

    added pic

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import {bandsDict} from './bandsDict'
 export default function App() {
   let [letter, setLetter]=useState()
   let [bands, setBands]=useState([])
-  let [bandName, setBandName]=useState("P.F.M.")
+  let [bandName, setBandName]=useState("none")
   function handleLetter(e){
     setLetter(e.target.value)
     setBands(bandsDict.find(item=>item.name===e.target.value).bands)
@@ -40,7 +40,7 @@ return (
         {bands.map(item=>
           <option key={item.name}>{item.name}</option>
         )}</select><br/>
- <ol>{topItems}</ol>
+      {bandName=="none"?<img style={{paddingTop:"25px"}} src="https://www.dropbox.com/scl/fi/vp8t07rwynl6fdc1bvozt/20-das-bedeutet-die-absage-fuer-die-veranstalter-16-9-spoton-article-744799.jpg?rlkey=gvffqqkdqzayj6okdgv30qjjg&raw=1"/>:<ol>{topItems}</ol>}
     </div>
   )
 }
